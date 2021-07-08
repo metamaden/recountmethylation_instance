467777e1fce26afaad635d9a19b1405082fd8849
Sean Maden <metamaden@users.noreply.github.com>
2021-07-08 13:00:10 -0700

    Update msrap_setup.sh

--- inst/scripts/msrap_setup.sh
+++ inst/scripts/msrap_setup.sh
@@ -13,11 +13,18 @@ python2 -m pip install numpy scipy scikit-learn setuptools marisa-trie dill
 python2 -m pip install rdflib xlsxwriter pygraphviz
 python2 -m pip install nltk==3.4
 python2 -c "import nltk; nltk.download('punkt')"
+
 # add pipeline dir paths to pythonpath
 PYTHONPATH=/MetaSRA-pipeline/:/MetaSRA-pipeline/bktree/:/MetaSRA-pipeline/map_sra_to_ontology/
-# run setup, inc. download obo ontology files
+
 cd ./MetaSRA-pipeline/setup_map_sra_to_ontology
-./setup.sh
+
+# clone bktree repo
+sudo rm -r bktree
+sudo git clone https://www.github.com/ahupp/bktree
+
+./setup.sh # run setup, inc. ontology downloads
+
 # test the pipeline
 cd -
 python2 ./MetaSRA-pipeline/run_pipeline.py --fnvread ./jsonexe --fnvwrite ./jsonexe.out
