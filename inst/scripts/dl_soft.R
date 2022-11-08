d32ea256915a8a1bc1294b0c4960581bc1d7a72d
Sean Maden <metamaden@users.noreply.github.com>
2022-11-08 10:55:29 -0800

    Update dl_soft.R
    adds try conditions for downloads
    organizes code for argparse, command line args
    organizes code for source() calls

--- inst/scripts/dl_soft.R
+++ inst/scripts/dl_soft.R
@@ -1,23 +1,52 @@
-dfp <- "temp/gse_temp"
+#!/usr/bin/env R
+
+
+# Author: Sean Maden
+#
+# Download GSE SOFT files using a diff from currently available files.
+#
+#
+
+# params/new function args
+dfp <- "gse_temp"
 base.url <- "ftp://ftp.ncbi.nlm.nih.gov/geo/series"
-# get paths
-eq.fname <- "gsequery_filt.1665946384"
 eq.dirpath <- file.path("equery")
 idat.dirpath <- file.path("idats")
 soft.dirpath <- file.path("gse_soft")
+rename <- TRUE
+verbose <- TRUE
+eq.str <- "gsequery_filt"
+
+# get new timestamp
+ts.str <- gsub("\\..*", "", as.character(as.numeric(Sys.time())))
+
+# get gsequery_filt latest
+lfv <- list.files(eq.dirpath)
+lfvf <- lfv[grepl(paste0(eq.str, ".*"), lfv)]
+tsv <- as.numeric(gsub(".*\\.", "", lfvf))
+which.latest <- tsv==max(tsv)
+eq.fname <- lfvf[which.latest]
+
 # get id vectors
 rl <- readLines(file.path(eq.dirpath, eq.fname))
 idv <- unlist(strsplit(rl, " "))
 which.gse <- grepl("GSE", idv)
 gsev <- idv[which.gse]
-length(gsev) # [1] 362
 gsmv <- idv[!which.gse]
-length(gsmv) # [1] 34995
+
+# check destination directory for new idats
+if(verbose){message("Checking download destination directory (dfp)...")}
+if(!dir.exists(dfp)){
+  message("Making new dest dir dfp.");tdir <- try(dir.create(dfp))
+  if(!tdir){
+    stop("Error, there was an issue making the new dest dir dfp.")}}
+
 # get id diffs
-idat.fn.dlv <- unique(gsub("\\..*", "", list.files(idat.dirpath)))
-gsmvf <- gsmv[!gsmv %in% idat.fn.dlv]
+#idat.fn.dlv <- unique(gsub("\\..*", "", list.files(idat.dirpath)))
+#gsmvf <- gsmv[!gsmv %in% idat.fn.dlv]
 soft.fn.dlv <- unique(gsub("\\..*", "", list.files(soft.dirpath)))
 gsevf <- gsev[!gsev %in% soft.fn.dlv]
+fnv.dl <- c()
 for(gsei in gsevf){
   # gsei <- "GSE161485"
   # get download url
@@ -26,8 +55,25 @@ for(gsei in gsevf){
   gse.dir.str <- paste0(gse.dir.str, paste0(rep("n", 3), collapse = ""))
   soft.fname <- paste0(gsei, "_family.soft.gz")
   dl.url <- file.path(base.url, gse.dir.str, gsei, "soft", soft.fname)
+  # try to ping server for valid filename
+  server.check.fn <- try(RCurl::getURL(dl.url, ftp.use.epsv = FALSE, dirlistonly = TRUE))
+  if(is(server.check.fn, "try-error")){message("Error querying server. Skipping."); next}
   # try download
-  fn <- RCurl::getURL(dl.url, ftp.use.epsv = FALSE, dirlistonly = TRUE)
   dest.fpath <- file.path(dfp, soft.fname)
-  utils::download.file(dl.url, dest.fpath)
+  trydl <- try(utils::download.file(dl.url, dest.fpath))
+  if(is(trydl, "try-error")){message("Error on SOFT download. Skipping."); next}
+  fnv.dl <- c(fnv.dl, soft.fname)
+  # try rename
+  if(rename){
+    if(file.exists(dest.fpath)){
+      new.fname <- paste0(c(gsei,ts.str,f), collapse = ".")
+      file.rename(dest.fpath, file.path(dfp, new.fname))
+    } else{
+      message("Couldn't find file to rename.")
+    }
+  }
 }
+
+message("From input of ",length(gsevf),
+        " GSE IDs, finished successful download of ", 
+        length(fnv.dl), " SOFT files.")
